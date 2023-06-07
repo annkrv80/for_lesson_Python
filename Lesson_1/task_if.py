d20c4b627af1dbc0c8518dec021f880b3d5291a8
Anna <annkrv80@yandex.ru>
2023-06-08 02:04:58 +0700

    the task #3 from LESSON_1 was complited

---
+++ Lesson_1/task_if.py
@@ -0,0 +1,8 @@
+my_match = int(input('2 + 2 = '))
+if 2 + 2 == my_match:
+    print('Верно')
+else:
+    print('Неверно')
+
+my_match = int(input('2 + 2 =  '))
+print('Верно' if 2 + 2 == my_match else 'Неверно')
