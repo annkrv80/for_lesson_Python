a9c08b66edca0f2d0d78f3409aa46537084440a0
Anna <annkrv80@yandex.ru>
2023-06-08 02:06:07 +0700

    the task #6 from LESSON_1 was complited

---
+++ Lesson_1/task_while02.py
@@ -0,0 +1,22 @@
+min_limit = 0
+max_limit = 10
+count = 3
+num = None
+
+while count > 0:
+    print('Попытка' , count)
+    count-=1
+
+    print('Введите число между ', min_limit, ' и ', max_limit)
+    num = float(input())
+    if num < min_limit or num > max_limit:
+        print('Неверно!')
+    else:
+        break
+
+
+else:
+    print('Исчерпаны попытки! Сожалею')
+    quit()
+
+print('Введено число ', num)
